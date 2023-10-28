5a3cd4bc0f7c3b2ea3232cbb99881fc4f4418542
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-10-28 19:25:04 +0600

    Booking patch added

--- index.js
+++ index.js
@@ -65,6 +65,19 @@ async function run() {
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
     });
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
     app.delete("/bookings/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
